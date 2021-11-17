1dc616e67bee9fc3ce427d6de900300db3960a2f
Andres Kwan <andreskwan@gmail.com>
2021-11-17 15:39:19 -0500

    [Day4] - adding tests for ranges of ages.

--- TDDHackerRankNew/TDDHackerRankTests/Day4ClassVsInstanceTest.swift
+++ TDDHackerRankNew/TDDHackerRankTests/Day4ClassVsInstanceTest.swift
@@ -30,7 +30,7 @@ class Day4ClassVsInstanceTest: XCTestCase {
         XCTAssertEqual(result, Expect)
     }
     
-    func test_whenUserGivesAgeUnder13_printYouAreYoung(){
+    func test_whenWeHave_ARange_AsImputOfAgesUnder13_printYouAreYoung(){
         let agesUnder13 = stride(from:1, through: 12, by: 1)
     
         let expectedValue = "You are young."
@@ -52,6 +52,18 @@ class Day4ClassVsInstanceTest: XCTestCase {
         XCTAssertEqual(result, Expect)
     }
     
+    func test_whenWeHave_ARange_AsImputOfAgesBetween13and18_printYouAreTeeneger(){
+        let agesBetween13and18 = stride(from:13, through: 18, by: 1)
+    
+        let expectedValue = "You are teeneger."
+        let people = agesBetween13and18.map { Person(initialAge: $0) }
+        
+        let result = people.map{ $0.amIOld()}
+        let older = result.first { $0 != expectedValue }
+        
+        XCTAssertNil(older)
+    }
+    
     func test_whenUserGivesAgeOver18_printYouAreOld(){
         let ageGiven = 20
         let Expect = "You are old."
@@ -62,14 +74,28 @@ class Day4ClassVsInstanceTest: XCTestCase {
         XCTAssertEqual(result, Expect)
     }
     
+    func test_whenWeHave_ARange_AsImputOfAgesBetween13and18_printYouAreOld(){
+        let agesOver18 = stride(from:19, through: 99, by: 1)
+    
+        let expectedValue = "You are old."
+        let people = agesOver18.map { Person(initialAge: $0) }
+        
+        let result = people.map{ $0.amIOld()}
+        let older = result.first { $0 != expectedValue }
+        
+        XCTAssertNil(older)
+    }
+    
     func test_whenUserGivesAge_ageIncreases(){
         let ageGiven = 12
-        let Expect = ageGiven+1
+        let expected = ageGiven + 1
         let sut = Person(initialAge: ageGiven)
         
+        //when
         sut.yearPasses()
         
-        XCTAssertEqual(sut.age, Expect)
+        //then
+        XCTAssertEqual(sut.age, expected)
     }
     
     
