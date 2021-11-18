69bee50d44ec77e22bb55836ffbec3f98b688409
Michael Rodriguez <miarodriguezur@unal.edu.co>
2021-11-18 18:16:49 -0500

    [Day 8] The logic test for the first test to pass is added in production code

--- TDDHackerRankNew/TDDHackerRank/Day8DictionariesAndMaps.swift
+++ TDDHackerRankNew/TDDHackerRank/Day8DictionariesAndMaps.swift
@@ -7,3 +7,17 @@
 
 import Foundation
 
+private let phoneBook = [
+    "sam" : "99912222",
+    "tom" : "11122222",
+    "harry" : "12299933"
+]
+
+func consultPhoneBook(nameKey: String) -> String {
+    guard !nameKey.isEmpty else {
+        return "fails, Empty name."
+    }
+    return ""
+}
+
+
