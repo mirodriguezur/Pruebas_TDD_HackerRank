97470b79c0b3687b2c4c6cb5dced615a8f1bc37d
Michael Rodriguez <miarodriguezur@unal.edu.co>
2021-11-19 10:54:22 -0500

    [Day 9] The logic for the First test to pass is added in production code.  Green Cycle

---
+++ TDDHackerRankNew/TDDHackerRank/Day9Recursion.swift
@@ -0,0 +1,13 @@
+//
+//  Day9Recursion.swift
+//  TDDHackerRank
+//
+//  Created by Michael Alexander Rodriguez Urbina on 19/11/21.
+//
+
+import Foundation
+
+func recursiveFactorial(number: Int) -> Int {
+    guard !(number == 0 || number == 1) else {return 1}
+    return 0
+}
