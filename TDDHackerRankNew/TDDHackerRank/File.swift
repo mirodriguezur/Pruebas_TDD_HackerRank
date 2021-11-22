e1f9e28c7830916ea425ab551e026a963c2bd6a4
Michael Rodriguez <miarodriguezur@unal.edu.co>
2021-11-22 15:50:41 -0500

    [Day 10] The logic for the first test to pass is added in production code.  Green cycle.

---
+++ TDDHackerRankNew/TDDHackerRank/File.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  TDDHackerRank
+//
+//  Created by Michael Alexander Rodriguez Urbina on 22/11/21.
+//
+
+import Foundation
+
+func maximunOnesConsecutivesInBinaryNumber(number: Int) -> Int {
+    
+    guard !(number < 0) else {
+        return 0
+    }
+    
+    return 1
+}
--- TDDHackerRankNew/TDDHackerRankTests/Day10BinaryNumbers.swift
+++ TDDHackerRankNew/TDDHackerRankTests/Day10BinaryNumbers.swift
@@ -6,20 +6,22 @@
 //
 
 import XCTest
+@testable import TDDHackerRank
 
 class Day10MaximumOnesConsecutiveInaBinaryNumberTest: XCTestCase {
 
     func test_whenUserProvidesNegativeNumber_printZero() {
         
-        let inputNumber = -4
+        let inputNumber = -3
         let expected = 0
         
         let result = maximunOnesConsecutivesInBinaryNumber(number: inputNumber)
         
-        XCTAssertTrue(inputNumber > 0, "Fails, It's a positive number")
+        XCTAssertTrue(inputNumber < 0, "Fails, It's a positive number")
         
         XCTAssertEqual(expected, result)
-        
     }
-    
+        
 }
+    
+
