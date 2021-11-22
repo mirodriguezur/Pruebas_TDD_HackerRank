9c526cfa7760cf04c5f19582ae1251c1a11625da
Michael Rodriguez <miarodriguezur@unal.edu.co>
2021-11-22 16:13:30 -0500

    [Day 10] The logic for the third test to pass is added in production Code. Green cycle.

--- TDDHackerRankNew/TDDHackerRank/Day10BinaryNumbers.swift
+++ TDDHackerRankNew/TDDHackerRank/Day10BinaryNumbers.swift
@@ -9,9 +9,25 @@ import Foundation
 
 func maximunOnesConsecutivesInBinaryNumber(number: Int) -> Int {
     
+    var inputNumber = number
+    var sum = 0
+    var max = 0
+    
     guard !(number <= 0) else {
         return 0
     }
     
-    return 1
+    while inputNumber > 0 {
+        if inputNumber % 2 == 1 {
+            sum += 1
+            if sum > max {
+                max = sum
+            }
+        } else {
+            sum = 0
+        }
+        inputNumber = inputNumber / 2
+    }
+    
+    return max
 }
