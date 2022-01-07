aaa2be5af24dba33a694067e36af6c03f0cf050b
Michael Rodriguez <miarodriguezur@unal.edu.co>
2022-01-07 12:38:33 -0500

    [Day 14] First test: test_whenUsersProvidesAnEmptyArray_throwsError is added.  Green cycle TDD

--- HackerRank_with_TDD/HackerRank_with_TDDTests/Day14Difference.swift
+++ HackerRank_with_TDD/HackerRank_with_TDDTests/Day14Difference.swift
@@ -8,14 +8,31 @@
 import XCTest
 
 class Day14Difference: XCTestCase {
+    
+    func test_whenUsersProvidesAnEmptyArray_throwsError() throws {
+        //given
+        
+        do {
+            // when
+            _ = try Difference(matriz: [])
+        } catch (let error) {
+            //then
+            XCTAssertEqual(error as! differenceErrors, differenceErrors.emptyInput)
+        }
+    }
 
+}
+
+enum differenceErrors: Error {
+    case emptyInput
 }
 
 class Difference {
     private var elements = [Int]()
     var maximunDifference: Int
     
-    init (matriz: Array<Int>){
+    init (matriz: Array<Int>) throws {
+        guard !(matriz.isEmpty) else {throw differenceErrors.emptyInput}
         elements = matriz
         maximunDifference = 0
     }
